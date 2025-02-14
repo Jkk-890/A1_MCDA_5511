f38c43085fc98cb9309b4dab868f648c4498ff6b
Ethan Cooke <ethandcooke@gmail.com>
2025-02-14 15:26:29 -0400

    Version 2

--- data_comparison.py
+++ data_comparison.py
@@ -18,10 +18,42 @@ def read_csv_embeddings(csv_file) -> dict:
     return embedding_dict
 
 
-origin_embeddings = read_csv_embeddings('person_embeddings.csv')
-modified_embeddings = read_csv_embeddings('person_embeddings_modified.csv') 
- 
-for key in origin_embeddings.keys():
-    if not np.array_equal(origin_embeddings[key], modified_embeddings[key]):
-        cosine = np.dot(origin_embeddings[key], modified_embeddings[key]) / (norm(origin_embeddings[key]) * norm(modified_embeddings[key]))
-        print(f'{key} - cosine similarity: {cosine}')
+def compare_embeddings(origin_embeddings, modified_embeddings) -> dict:
+    distances = {}
+    for key in origin_embeddings.keys():
+        if not np.array_equal(origin_embeddings[key], modified_embeddings[key]):
+            cosine = np.dot(origin_embeddings[key], modified_embeddings[key]) / (norm(origin_embeddings[key]) * norm(modified_embeddings[key]))
+            distances[key] = cosine
+            
+    return distances
+
+
+def print_distance_stats(distances: dict, name=""):
+    cosine_min = np.min(list(distances.values()))
+    cosine_max = np.max(list(distances.values()))
+    
+    min_key = [key for key, value in distances.items() if value == cosine_min][0]
+    max_key = [key for key, value in distances.items() if value == cosine_max][0]
+    mean = np.mean(list(distances.values()))
+    
+    print(f'\n{name.capitalize()}')
+    print(f'\nMean cosine similarity: {mean}')
+    print(f'Min cosine similarity - {min_key}: {cosine_min}')
+    print(f'Max cosine similarity - {max_key}: {cosine_max}')
+    
+
+embeddings_path = os.path.join(os.getcwd(), 'embeddings')
+
+
+origin_embeddings = read_csv_embeddings(os.path.join(embeddings_path, 'person_embeddings.csv'))
+synonym_embeddings = read_csv_embeddings(os.path.join(embeddings_path, 'person_embeddings_synonym.csv')) 
+antonym_embeddings = read_csv_embeddings(os.path.join(embeddings_path, 'person_embeddings_antonym.csv'))
+rephrase_embeddings = read_csv_embeddings(os.path.join(embeddings_path, 'person_embeddings_rephrase.csv'))
+
+synonym_distances = compare_embeddings(origin_embeddings, synonym_embeddings)
+antonym_distances = compare_embeddings(origin_embeddings, antonym_embeddings)
+rephrase_distances = compare_embeddings(origin_embeddings, rephrase_embeddings)
+
+print_distance_stats(synonym_distances, "synonym")
+print_distance_stats(antonym_distances, "antonym")
+print_distance_stats(rephrase_distances, "rephrase")
